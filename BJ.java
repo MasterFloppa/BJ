8d46196595a2fbafa24c5f431f9bad502a77c590
Samrat <samratgupta834@gmail.com>
2023-08-03 00:17:50 +0530

    Update BJ.java

--- BJ.java
+++ BJ.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-//import java.util.concurrent.TimeUnit;
-
 public class BJ {
 	private static final int Height = 195;
 	private static final int Width = 139;
